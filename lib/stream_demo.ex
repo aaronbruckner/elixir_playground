240b99138f465ffdd09aa576fd0481e0d5ff91fb
Aaron Bruckner <aaronbruckner@gmail.com>
2023-06-18 21:45:18 -0700

    stream tests

--- lib/stream_demo.ex
+++ lib/stream_demo.ex
@@ -5,4 +5,17 @@ defmodule StreamDemo do
     |> Stream.map(fn line -> String.length(line) end)
     |> Enum.to_list()
   end
+
+  def longest_line_length(path) do
+    Enum.max([0 | line_lengths(path)])
+  end
+
+  @spec longest_line(Path.t()) :: String.t()
+  def longest_line(path) do
+    {_length, longestLine} = File.stream!(path)
+    |> Stream.map(fn line -> {String.length(line), line} end)
+    |> Enum.max_by(&(elem(&1, 0)))
+
+    longestLine
+  end
 end
--- test/stream_demo_test.exs
+++ test/stream_demo_test.exs
@@ -2,8 +2,20 @@ defmodule StreamDemoTest do
   use ExUnit.Case
   doctest StreamDemo
 
-  test "greet_person returns the right response" do
+  test "line_lengths returns list of line lengths" do
     assert StreamDemo.line_lengths("test/mockFiles/mockShortFile.txt") === [22, 5, 7, 53, 14]
+  end
+
+  test "longest_line_length returns longest line" do
+    assert StreamDemo.longest_line_length("test/mockFiles/mockShortFile.txt") === 53
+  end
+
+  test "longest_line_length returns 0 for empty file" do
+    assert StreamDemo.longest_line_length("test/mockFiles/mockEmptyFile.txt") === 0
+  end
+
+  test "longest_line returns the text for the longest line in the file" do
+    assert StreamDemo.longest_line("test/mockFiles/mockShortFile.txt") === "There once was a man name Fred and no one liked him.\n"
   end
 
 end
