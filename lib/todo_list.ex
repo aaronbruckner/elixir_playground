65413da3916180ef3c7f49efa0ca8590fc25fba9
Aaron Bruckner <aaronbruckner@gmail.com>
2023-06-19 12:12:40 -0700

    added types to todo list module

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,11 +1,16 @@
 defmodule TodoList do
 
+  @type t :: %{}
+
+  @spec new() :: t()
   def new(), do: %{}
 
+  @spec add_entry(t(), Calendar.date(), String.t()) :: t()
   def add_entry(todoList, date, title) do
     Map.update(todoList, date, [title], fn items -> [title | items] end)
   end
 
+  @spec entries(t(), Calendar.date()) :: list(String.t())
   def entries(todoList, date) do
     Map.get(todoList, date, [])
   end
