4f0600bc47eea4be96ac63e137e01cf524875196
Aaron Bruckner <aaronbruckner@gmail.com>
2023-06-20 09:58:39 -0700

    added update entry functionality to todo list

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -2,14 +2,24 @@ defmodule TodoList do
 
   defstruct next_id: 0, entries: %{}
 
+  @type newTodoEntry :: %{title: String.t(), date: Calendar.date()}
+
   @spec new() :: %TodoList{}
   def new(), do: %TodoList{}
 
-  @spec add_entry(%TodoList{}, %{title: String.t(), date: Calendar.date()}) :: %TodoList{}
+
+  @spec add_entry(%TodoList{}, newTodoEntry()) :: %TodoList{}
   def add_entry(todoList, entry) do
     entry = Map.put(entry, :id, todoList.next_id)
     entires = Map.put(todoList.entries, todoList.next_id, entry)
     %TodoList{todoList | entries: entires, next_id: todoList.next_id + 1}
+  end
+
+  @spec update_entry(%TodoList{}, number(), newTodoEntry()) :: %TodoList{}
+  def update_entry(todoList, id, entry) do
+    entry = Map.put(entry, :id, id)
+    entries = Map.update!(todoList.entries, id, fn _ -> entry end)
+    %TodoList{todoList | entries: entries}
   end
 
   @spec entries(%TodoList{}, Calendar.date()) :: list(String.t())
--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -34,4 +34,22 @@ defmodule TodoListTest do
     assert TodoList.entries(todoList, ~D[2020-01-02]) === [%{date: ~D[2020-01-02], title: "todo 2", id: 1}]
   end
 
+  test "update_entry - finds exisitng entry and updates it" do
+    todoList = TodoList.new()
+    |> TodoList.add_entry(%{date: ~D[2020-01-01], title: "todo 1"})
+    |> TodoList.add_entry(%{date: ~D[2020-01-02], title: "todo 2"})
+
+    [entry | _] = TodoList.entries(todoList, ~D[2020-01-01])
+
+    todoList = TodoList.update_entry(todoList, entry.id, %{date: ~D[2020-01-03], title: "todo 3"})
+
+    assert TodoList.entries(todoList, ~D[2020-01-01]) === []
+    assert TodoList.entries(todoList, ~D[2020-01-03]) === [%{date: ~D[2020-01-03], title: "todo 3", id: 0}]
+  end
+
+  test "update_entry - throws error if id doesn't exist" do
+    assert_raise KeyError, fn -> TodoList.new()
+    |> TodoList.update_entry(99, %{date: ~D[2020-01-01], title: "todo 1"}) end
+
+  end
 end
